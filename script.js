d404be4e71ecda65e40fbd5ab459bf97a9bda022
Jessica <jess.newman19@gmail.com>
2022-03-15 10:07:49 -0400

    added gitHub link and event listener

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ hover.addEventListener("mouseenter", function(event) {
     }, 1000);
     }, false);
 
+
+    document.querySelector('#github > a').id = "gitHub"; 
+    let mouseOver = document.getElementById("gitHub");
+    mouseOver.addEventListener("mouseenter", function (event) { 
+        event.target.style.color = "purple"; 
+        setTimeout(function() { 
+            event.target.style.color = "";
+        }, 1000);
+    }, false);
+
